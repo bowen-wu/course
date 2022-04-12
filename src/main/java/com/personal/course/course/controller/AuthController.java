e92798abcaa8cfc283c20e3cbaf3f47e8b90f97b
bowen-wu <13793248162@163.com>
2022-04-12 16:00:19 +0800

    feat: config sping database

---
+++ src/main/java/com/personal/course/course/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.personal.course.course.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class AuthController {
+    @GetMapping("/test")
+    public String test() {
+        return "Hello World!";
+    }
+}
