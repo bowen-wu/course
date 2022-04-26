5497534ba83e59e8ede152263ada944f86cc2e51
bowen-wu <13793248162@163.com>
2022-04-26 17:38:05 +0800

    feat: add AbstractIntegrationTest

---
+++ src/test/java/com/personal/course/controller/AbstractIntegrationTest.java
@@ -0,0 +1,90 @@
+package com.personal.course.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.personal.course.CourseApplication;
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpRequest.Builder;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = CourseApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {"spring.config.location=classpath:test-application.properties"})
+public abstract class AbstractIntegrationTest {
+    @LocalServerPort
+    private int port;
+
+    @Value("${spring.datasource.url}")
+    private String flywayUrl;
+    @Value("${spring.datasource.username}")
+    private String flywayUser;
+    @Value("${spring.datasource.password}")
+    private String flywayPassword;
+
+    public final ObjectMapper objectMapper = new ObjectMapper();
+    public final HttpClient client = HttpClient.newBuilder().build();
+
+    @BeforeEach
+    public void setUp() {
+        objectMapper.findAndRegisterModules();
+        Flyway flyway = Flyway.configure().dataSource(flywayUrl, flywayUser, flywayPassword).load();
+        flyway.clean();
+        flyway.migrate();
+    }
+
+    public HttpResponse<String> post(String uri, BodyPublisher bodyPublisher, String... headers) throws IOException, InterruptedException {
+        return getHttpRequest(uri, "POST", bodyPublisher, headers);
+    }
+
+    public HttpResponse<String> delete(String uri, String... headers) throws IOException, InterruptedException {
+        return getHttpRequest(uri, "DELETE", BodyPublishers.noBody(), headers);
+    }
+
+    public HttpResponse<String> get(String uri) throws IOException, InterruptedException {
+        return getHttpRequest(uri, "GET", BodyPublishers.noBody());
+    }
+
+    public HttpResponse<String> get(String uri, String... headers) throws IOException, InterruptedException {
+        return getHttpRequest(uri, "GET", BodyPublishers.noBody(), headers);
+    }
+
+    public HttpResponse<String> getHttpRequest(
+            String uri,
+            String method,
+            BodyPublisher bodyPublisher,
+            String... headers) throws IOException, InterruptedException {
+        return getHttpRequest(uri, method, bodyPublisher, BodyHandlers.ofString(), headers);
+    }
+
+    public <T> HttpResponse<T> getHttpRequest(
+            String uri,
+            String method,
+            BodyPublisher bodyPublisher,
+            HttpResponse.BodyHandler<T> responseBodyHandler,
+            String... headers) throws IOException, InterruptedException {
+        HttpRequest request;
+        Builder builder = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:" + port + "/api/v1" + uri))
+                .method(method, bodyPublisher);
+        if (headers.length > 0) {
+            request = builder.headers(headers).build();
+        }
+        request = builder.build();
+
+        return client.send(request, responseBodyHandler);
+    }
+}
--- src/test/java/com/personal/course/controller/AuthIntegrationTest.java
+++ src/test/java/com/personal/course/controller/AuthIntegrationTest.java
@@ -1,79 +1,29 @@
 package com.personal.course.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.personal.course.CourseApplication;
 import com.personal.course.entity.Response;
 import com.personal.course.entity.Status;
 import com.personal.course.entity.User;
-import org.flywaydb.core.Flyway;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = CourseApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestPropertySource(properties = {"spring.config.location=classpath:test-application.properties"})
-class AuthIntegrationTest {
-    @LocalServerPort
-    private int port;
-
-    @Value("${spring.datasource.url}")
-    private String flywayUrl;
-    @Value("${spring.datasource.username}")
-    private String flywayUser;
-    @Value("${spring.datasource.password}")
-    private String flywayPassword;
-
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
-    @BeforeEach
-    public void setUp() {
-        objectMapper.findAndRegisterModules();
-        Flyway flyway = Flyway.configure().dataSource(flywayUrl, flywayUser, flywayPassword).load();
-        flyway.clean();
-        flyway.migrate();
-    }
-
+class AuthIntegrationTest extends AbstractIntegrationTest {
     @Test
     public void testLoginProcess() throws IOException, InterruptedException {
-        HttpClient client = HttpClient.newBuilder().build();
-
         // 检查登录状态 GET /session => 401
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:" + port + "/api/v1/session"))
-                .GET()
-                .build();
-
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        HttpResponse<String> response = get("/session");
         assertEquals(401, response.statusCode());
 
         // 注册 POST /user => 201
         String usernameAndPassword = "username=jackson&password=jackson";
-        request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:" + port + "/api/v1/user"))
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-                .POST(BodyPublishers.ofString(usernameAndPassword))
-                .build();
-
-        response = client.send(request, BodyHandlers.ofString());
+        response = post("/user", BodyPublishers.ofString(usernameAndPassword), HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE);
         assertEquals(201, response.statusCode());
         Response<User> registerResponse = objectMapper.readValue(response.body(), new TypeReference<>() {
         });
@@ -81,12 +31,7 @@ class AuthIntegrationTest {
         assertEquals(Status.OK, registerResponse.getData().getStatus());
 
         // 登录 POST /session => 200 + User
-        request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:" + port + "/api/v1/session"))
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-                .POST(BodyPublishers.ofString(usernameAndPassword))
-                .build();
-        response = client.send(request, BodyHandlers.ofString());
+        response = post("/session", BodyPublishers.ofString(usernameAndPassword), HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE);
 
         assertEquals(200, response.statusCode());
         Response<User> loginResponse = objectMapper.readValue(response.body(), new TypeReference<>() {
@@ -97,13 +42,7 @@ class AuthIntegrationTest {
         String cookie = response.headers().allValues(HttpHeaders.SET_COOKIE).get(0);
 
         // 检查登录状态 GET /session => 200 + User
-        request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:" + port + "/api/v1/session"))
-                .setHeader(HttpHeaders.COOKIE, cookie)
-                .GET()
-                .build();
-
-        response = client.send(request, BodyHandlers.ofString());
+        response = get("/session", HttpHeaders.COOKIE, cookie);
         assertEquals(200, response.statusCode());
         Response<User> loginStatusResponse = objectMapper.readValue(response.body(), new TypeReference<>() {
         });
@@ -112,23 +51,11 @@ class AuthIntegrationTest {
         assertEquals(loginResponse.getData().getStatus(), loginStatusResponse.getData().getStatus());
 
         // 登出 DELETE /session => 204
-        request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:" + port + "/api/v1/session"))
-                .setHeader(HttpHeaders.COOKIE, cookie)
-                .DELETE()
-                .build();
-
-        response = client.send(request, BodyHandlers.ofString());
+        response = delete("/session", HttpHeaders.COOKIE, cookie);
         assertEquals(204, response.statusCode());
 
         // 检查登录状态 GET /session => 401
-        request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:" + port + "/api/v1/session"))
-                .setHeader(HttpHeaders.COOKIE, cookie)
-                .GET()
-                .build();
-
-        response = client.send(request, BodyHandlers.ofString());
+        response = get("/session", HttpHeaders.COOKIE, cookie);
         assertEquals(401, response.statusCode());
     }
 
